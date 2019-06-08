1dae2f66300c487d237f0bf35df378ec0612de52
Mario Adamo Jr <marioadamojr@gmail.com>
2019-06-08 16:20:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,10 @@ class Application
     num_3 = Kernel.rand(1..20)
 
     if num_1==num_2 && num_1==num_3
-
+      resp.write "You Win"
+    else
+      resp.write "You Lose"
+    end
     resp.finish
   end
 
